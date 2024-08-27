39d9c8318af3b760661054b9d36be9c80ce562ad
Pa Khan <don@pa-khan.com>
2024-08-27 16:54:37 +0300

    Status

---
+++ status/status.go
@@ -0,0 +1,22 @@
+package status
+
+type Status = uint16
+
+const (
+  Success Status = iota // Успешно
+  Any  // Любая ошибка
+  NotFound  // Не найдено
+  PermissionDenied  // Недостаточно прав
+  TooFrequentRequests  // Слишком частые запросы
+  InternalError  // Внутренняя ошибка сервера
+  ManyConnections  // Много подключений
+  NotEnoughArguments  // Недостаточно аргументов
+  NotAuthorized  // Не авторизован
+  Authorized  // Уже авторизован
+  IncorrectValue  // Неверное значение
+  Inactivity  // Бездействие
+  Timeout  // Время ожидания истекло
+  InvalidInput  // Неверный ввод
+  ResourceUnavailable  // Ресурс недоступен
+  OperationFailed  // Операция не удалась
+)
