9d999aa3ea3ae1b561a1f24b617ee68c3f8a9a05
Pa Khan <don@pa-khan.com>
2024-09-17 17:19:27 +0300

    Update go submodule

--- status/status.go
+++ status/status.go
@@ -20,3 +20,25 @@ const (
   ResourceUnavailable  // Ресурс недоступен
   OperationFailed  // Операция не удалась
 )
+var m = map[Status]string{
+  Success:"Успешно",
+  Any:"Любая ошибка",
+  NotFound:"Не найдено",
+  PermissionDenied:"Недостаточно прав",
+  TooFrequentRequests:"Слишком частые запросы",
+  InternalError:"Внутренняя ошибка сервера",
+  ManyConnections:"Много подключений",
+  NotEnoughArguments:"Недостаточно аргументов",
+  NotAuthorized:"Не авторизован",
+  Authorized:"Уже авторизован",
+  IncorrectValue:"Неверное значение",
+  Inactivity:"Бездействие",
+  Timeout:"Время ожидания истекло",
+  InvalidInput:"Неверный ввод",
+  ResourceUnavailable:"Ресурс недоступен",
+  OperationFailed:"Операция не удалась",
+}
+
+func readable(s Status) string {
+  return m[s]
+}
