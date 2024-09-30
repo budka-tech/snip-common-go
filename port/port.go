497e076947be3ff65b78b4d35245df6fc05aeb60
Pa Khan <don@pa-khan.com>
2024-09-30 19:39:35 +0300

    FormatLocal

--- port/port.go
+++ port/port.go
@@ -9,7 +9,8 @@ type Port = uint16
 const (
 	Users  Port = 10000
 	Gsm         = 10100
-	Assist      = 10200
+	Push        = 10100
+	Assist      = 10300
 )
 
 func Format(port Port) string {
@@ -18,4 +19,8 @@ func Format(port Port) string {
 
 func FormatTCP(port Port) string {
 	return fmt.Sprintf(":%d", port)
+}
+
+func FormatLocal(port Port) string {
+	return fmt.Sprintf("localhost:%d", port)
 }
