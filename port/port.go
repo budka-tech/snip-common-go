1bb7760a72762f355ff823105e432fc2834202a3
Pa Khan <don@pa-khan.com>
2024-12-16 07:29:11 +0300

    Ports для k8s

--- port/port.go
+++ port/port.go
@@ -2,6 +2,7 @@ package port
 
 import (
 	"fmt"
+	"github.com/budka-tech/envo"
 )
 
 type Port = uint16
@@ -16,12 +17,39 @@ const (
 	S3Grpc          = 10500
 )
 
+type ServiceName string
+
+const (
+	ServiceUsers  ServiceName = "snip-users-service"
+	ServiceGsm                = "snip-gsm-service"
+	ServicePush               = "snip-push-service"
+	ServiceAssist             = "snip-assist-service"
+	ServiceS3G                = "snip-s3-service"
+)
+
+var m = map[ServiceName]Port{
+	ServiceUsers:  UsersGrpc,
+	ServiceGsm:    GsmGrpc,
+	ServicePush:   PushGrpc,
+	ServiceAssist: AssistGrpc,
+	ServiceS3G:    S3Grpc,
+}
+
 func Format(port Port) string {
 	return fmt.Sprintf("%d", port)
 }
 
 func FormatTCP(port Port) string {
 	return fmt.Sprintf(":%d", port)
+}
+
+func FormatServiceTCP(env *envo.Env, name ServiceName) string {
+	port := m[name]
+	if env.IsLocal() {
+		return FormatLocal(port)
+	} else {
+		return fmt.Sprintf("%v:%d", name, port)
+	}
 }
 
 func FormatLocal(port Port) string {
